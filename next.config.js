2a087f232a00d64886d8d4b6435a42c376d3720b
SpearShard <gjayaditya20@gmail.com>
2025-10-01 18:12:26 +0530

    Fixed the scrolling issue, and chnaged some content

--- next.config.js
+++ next.config.js
@@ -1,24 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",
-    basePath: process.env.NODE_ENV === 'production' ? "" : "",
-    images: {
-      unoptimized: false,
-      domains: ['drive.google.com'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'ik.imagekit.io',
-          pathname: '/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'drive.google.com',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  basePath: process.env.NODE_ENV === 'production' ? "" : "",
+  images: {
+    unoptimized: false,
+    domains: [
+      'drive.google.com',
+      'res.cloudinary.com', // 👈 add here for backwards-compat
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', // 👈 add Cloudinary here too
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
